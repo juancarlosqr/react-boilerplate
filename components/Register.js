cef834e5cab7df5e7ccbd53905884573acdd2bfb
Juan Carlos Quintero <jcquintero.ce@gmail.com>
2015-11-06 18:16:49 -0300

    Changing state

--- components/Register.js
+++ components/Register.js
@@ -1,15 +1,35 @@
 var React = require('react');
 
 var Register = React.createClass({
-  clickHandler: function() {
-    alert('Great! Now you are registered');
+  getInitialState: function () {
+    return {
+      count: 57,
+      registered: false
+    }
+  },
+  clickHandler: function(e) {
+    e.preventDefault();
+    if (this.state.registered) {
+      alert('Sorry...! You are already registered');
+    } else {
+      this.setState({
+        count: this.state.count + 1,
+        registered: true
+      });
+      setTimeout(function () {
+        alert('Great! Now you are registered');
+      }, 1);
+    }
   },
   render: function() {
     var url = "http://facebook.github.io/react";
     return (
-      <p>
-        <a href={url} className="btn" target="_blank" onClick={this.clickHandler}>Register</a>
-      </p>
+      <section>
+        <p>
+          <a href={url} className="btn" target="_blank" onClick={this.clickHandler}>Register</a>
+        </p>
+        <p>We have {this.state.count} people register so far. That&apos;s great!</p>
+      </section>
     );
   }
 });
--- components/Title.js
+++ components/Title.js
@@ -2,13 +2,23 @@ var React = require('react');
 var Subtitle = require('./Subtitle');
 
 var Title = React.createClass({
+  getInitialState: function() {
+    return {
+      text: 'A place for React people'
+    }
+  },
+  onClickHandler: function(e) {
+    this.setState({text: 'Sorry... there are no Speakers yet'});
+  },
   render: function() {
     var title = "ReactConf";
-    var subtitle = "A place for React people";
     return (
       <div>
         <h1>{title}</h1>
-        <Subtitle text={subtitle} />
+        <Subtitle text={this.state.text} />
+        <div>
+          <input type="button" className="btn" onClick={this.onClickHandler} value="Speakers" />
+        </div>
       </div>
     );
   }
